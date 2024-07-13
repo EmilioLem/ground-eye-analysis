1a581e01b4cc70ea27a06bcf9c92fc775d1c4c05
EmilioLem <emilio21lemus@gmail.com>
2024-07-13 16:40:43 -0600

    Ditched images completely, now working with canvas only

--- script/main.js
+++ script/main.js
@@ -57,10 +57,10 @@ toggleCameraBtn.addEventListener('click', async () => {
 
 measurementBtn.addEventListener('click', async () => {
   console.time('Total time');
-  const xSize = videoElement.videoWidth;
-  const ySize = videoElement.videoHeight;
-  const offscreen = new OffscreenCanvas(xSize, ySize);
-  const context = offscreen.getContext('2d');
+  const canvas = document.getElementById("canvas");
+  const xSize = canvas.width = videoElement.videoWidth;
+  const ySize = canvas.height = videoElement.videoHeight;
+  const context = canvas.getContext('2d');
 
   if (isCameraOn && videoElement.readyState >= 2) {
     console.time('Draw Image');
@@ -71,11 +71,6 @@ measurementBtn.addEventListener('click', async () => {
     const imageData = context.getImageData(0, 0, xSize, ySize);
     const data = imageData.data;
     console.timeEnd('Get ImageData');
-
-    console.time('Array Copy');
-    var flatData = data.slice();//Consider removing it entirely!!! Maybe we don't need a copy.
-    console.timeEnd('Array Copy');
-    //console.log(flatData);
 
     console.time('Modify ImageData');
     for (let i = 0; i < data.length; i += 4) {
@@ -91,25 +86,10 @@ measurementBtn.addEventListener('click', async () => {
     console.timeEnd('Put ImageData');
 
 
-    //drawBicolorDashedRect(context, 1030, 1030, 50, 50, 'white', 'black', 20);
-    //drawHollowRect(context, 0, 0, 200, 300, 'grey', 15);
-    //drawHollowRect(context, 0, 0, 190, 290, 'white', 5);
-    drawSimpleBoundingBox(context, 3, 3, 200, 200, 15, "blue");
+    drawSimpleBoundingBox(context, 0, 0, 130, 130, 15, "black");
+    drawSimpleBoundingBox(context, 0, 150, 130, 130, 15, "white");
 
-    console.time('Convert to Blob');
-    const blob = await offscreen.convertToBlob();
-    console.timeEnd('Convert to Blob');
-
-    console.time('Create Image');
-    const img = new Image();
-    img.src = URL.createObjectURL(blob);
-    document.getElementById("newImages").replaceChildren(img) ; //appendChild(img); //Correct??? yeah, maybe,hopefully
-    console.timeEnd('Create Image');
-
-    //Blobs are... dangerous 
-    //Or maybe not... 
-    //(I oppened the image url on another page, then zoomed in, and when I came back to the page... the image had scaled up too!!!)
-    //But apparently the blob-image was not responsive to the page size.
+    canvas.style.display='block';
   }
   console.timeEnd('Total time');
 });
@@ -123,52 +103,3 @@ function drawSimpleBoundingBox(ctx, x, y, width, height, lineWidth, color){
   ctx.fillRect(x, y, lineWidth, height); //Left bar
   ctx.fillRect(x+width-lineWidth, y, lineWidth, height); //Right bar
 }
-
-/*function drawHollowRect(ctx, x, y, width, height, color, lineWidth) {
-  ctx.strokeStyle = color;
-  ctx.lineWidth = lineWidth;
-  ctx.strokeRect(x, y, width, height);
-}
-
-function drawBicolorDashedRect(ctx, x, y, width, height, color1, color2, dashLength) {
-  ctx.lineWidth = 5;
-
-  // Function to draw a single dash
-  function drawDash(startX, startY, endX, endY, color) {
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.stroke();
-  }
-
-  // Helper function to draw dashes along a side
-  function drawSide(startX, startY, endX, endY, length, horizontal) {
-    let distance = horizontal ? Math.abs(endX - startX) : Math.abs(endY - startY);
-    for (let i = 0; i < distance; i += dashLength * 2) {
-      let dashStartX = horizontal ? startX + i : startX;
-      let dashEndX = horizontal ? Math.min(startX + i + dashLength, endX) : startX;
-      let dashStartY = horizontal ? startY : startY + i;
-      let dashEndY = horizontal ? startY : Math.min(startY + i + dashLength, endY);
-      drawDash(dashStartX, dashStartY, dashEndX, dashEndY, color1);
-
-      dashStartX = horizontal ? startX + i + dashLength : startX;
-      dashEndX = horizontal ? Math.min(startX + i + dashLength * 2, endX) : startX;
-      dashStartY = horizontal ? startY : startY + i + dashLength;
-      dashEndY = horizontal ? startY : Math.min(startY + i + dashLength * 2, endY);
-      drawDash(dashStartX, dashStartY, dashEndX, dashEndY, color2);
-    }
-  }
-
-  // Draw top side
-  drawSide(x, y, x + width, y, width, true);
-
-  // Draw right side
-  drawSide(x + width, y, x + width, y + height, height, false);
-
-  // Draw bottom side
-  drawSide(x + width, y + height, x, y + height, width, true);
-
-  // Draw left side
-  drawSide(x, y + height, x, y, height, false);
-}*/
